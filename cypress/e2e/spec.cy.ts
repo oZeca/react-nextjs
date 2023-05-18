676da962d3c5396a96d8c4684ec7fd4ee4b0e7f5
oZeca <ze.lupicaetano@gmail.com>
2023-05-18 22:23:31 +0100

    adds cypress test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Navigation', () => {
+  it('should navigate to the about page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/');
+
+    // Find a link with an href attribute containing "about" and click it
+    cy.get('a[href*="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"]').click();
+
+    // The new url should include "/about"
+    cy.url().should('include', 'vercel.com');
+  });
+});
